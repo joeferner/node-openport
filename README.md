6f1102edf1d1678238af4569549958bdc2b2c660
Joe Ferner <joe@fernsroth.com>
2012-02-22 17:38:59 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# openport
+
+Finds open network ports.
+
+## Installation
+
+```bash
+$ npm install openport
+```
+
+## Quick Examples
+
+```javascript
+var op = require('openport');
+
+op.find(function(err, port) {
+  if(err) { console.log(err); return; }
+  // yea! we have an open port.
+});
+
+op.find(
+  {
+    ports: [ 1024, 1025, 1026, 1028 ],
+    count: 2
+  }
+  function(err, ports) {
+    if(err) { console.log(err); return; }
+    // yea! we have two open ports.
+  }
+);
+
+op.find(
+  {
+    startingPort: 1024,
+    endingPort: 2000
+  }
+  function(err, port) {
+    if(err) { console.log(err); return; }
+    // yea! we have an open port between 1024 and 2000.
+  }
+);
+```
---
+++ test/openportTest.js
@@ -0,0 +1,142 @@
+'use strict';
+
+var net = require('net');
+var op = require('../');
+
+module.exports = {
+  'find a single open port': function (test) {
+    op.find(
+      {
+        ports: [ 1024 ]
+      },
+      function (err, port) {
+        test.ok(!err);
+        test.equals(1024, port);
+        test.done();
+      });
+  },
+
+  'port that requires sudo': function (test) {
+    op.find(
+      {
+        ports: [ 1023 ]
+      },
+      function (err, port) {
+        test.equals('no ports found', err.message);
+        test.ok(!port);
+        test.done();
+      });
+  },
+
+  'find a port after a closed port': function (test) {
+    var server = net.createServer();
+    server.listen(1024, function () {
+      op.find(
+        {
+          ports: [ 1024, 1025 ]
+        },
+        function (err, port) {
+          test.ok(!err);
+          test.equals(1025, port);
+          server.close();
+          test.done();
+        });
+    });
+  },
+
+  'find multiple open ports': function (test) {
+    op.find(
+      {
+        ports: [ 1024, 1025 ],
+        count: 2
+      },
+      function (err, ports) {
+        test.ok(!err);
+        test.equals(2, ports.length);
+        test.equals(1024, ports[0]);
+        test.equals(1025, ports[1]);
+        test.done();
+      });
+  },
+
+  'find open defaults': function (test) {
+    op.find(
+      function (err, port) {
+        test.ok(!err);
+        test.equals(1024, port);
+        test.done();
+      });
+  },
+
+  'find open with starting port': function (test) {
+    op.find(
+      {
+        startingPort: 10000
+      },
+      function (err, port) {
+        test.ok(!err);
+        test.equals(10000, port);
+        test.done();
+      });
+  },
+
+  'bad starting and ending port': function (test) {
+    try {
+      op.find(
+        {
+          startingPort: 1025,
+          endingPort: 1024
+        },
+        function (err, port) {
+          test.fail("this shouldn't happen");
+        });
+    } catch (ex) {
+      test.done();
+    }
+  },
+
+  'bad starting port': function (test) {
+    try {
+      op.find(
+        {
+          startingPort: 0
+        },
+        function (err, port) {
+          test.fail("this shouldn't happen");
+        });
+    } catch (ex) {
+      test.done();
+    }
+  },
+
+  'bad ending port': function (test) {
+    try {
+      op.find(
+        {
+          startingPort: op.maxPort + 1
+        },
+        function (err, port) {
+          test.fail("this shouldn't happen");
+        });
+    } catch (ex) {
+      test.done();
+    }
+  },
+
+  'find no open port hitting ending port': function (test) {
+    var server = net.createServer();
+    server.listen(1024, function () {
+      op.find(
+        {
+          startingPort: 1024,
+          endingPort: 1024
+        },
+        function (err, port) {
+          test.equals('no ports found', err.message);
+          test.ok(!port);
+          server.close();
+          test.done();
+        });
+    });
+  }
+};
